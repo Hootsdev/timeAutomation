fe2be65c043fad6f214cec13ad1a563bcae55bf1
Mark <email@example.com>
2022-08-30 17:59:34 +0300

    imp

---
+++ src/test/java/ut/com/deadspace/ext/jira/webwork/configurationPageActionTest.java
@@ -0,0 +1,34 @@
+package ut.com.deadspace.ext.jira.webwork;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import com.deadspace.ext.jira.webwork.configurationPageAction;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @since 3.5
+ */
+public class configurationPageActionTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test(expected=Exception.class)
+    public void testSomething() throws Exception {
+
+        //configuration-pageAction testClass = new configuration-pageAction();
+
+        throw new Exception("configuration-pageAction has no tests!");
+
+    }
+
+}
